8975f91d9fe52ca34b2d495fcdce360ed88c3adc
Prince <maihoonprinx@gmail.com>
2024-05-02 09:44:07 +0530

    logo changed

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -7,6 +7,7 @@ import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import LSvg from "../../../components/svgs/L";
 
 const LoginPage = () => {
 	const [formData, setFormData] = useState({
@@ -58,11 +59,11 @@ const LoginPage = () => {
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
 			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className='lg:w-2/3 fill-white' />
+				<LSvg className='lg:w-2/3 fill-white' />
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<XSvg className='w-24 lg:hidden fill-white' />
+					<LSvg className='w-24 lg:hidden fill-white' />
 					<h1 className='text-4xl font-extrabold text-white'>{"Let's"} go.</h1>
 					<label className='input input-bordered rounded flex items-center gap-2'>
 						<MdOutlineMail />
